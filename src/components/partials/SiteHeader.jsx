1da9d0a0a301e98f7cf353f1333f6ffa06880431
Kendrick Winata <kendrickwinata@outlook.com>
2022-11-20 16:09:31 +0800

    setup gh-pages

--- src/components/partials/SiteHeader.jsx
+++ src/components/partials/SiteHeader.jsx
@@ -4,108 +4,105 @@ import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 
 function logoutHandler(e) {
-  e.preventDefault();
-  localStorage.removeItem("user_token");
+	e.preventDefault();
+	localStorage.removeItem("user_token");
 }
 
 function SiteHeader() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const token = localStorage.getItem("user_token");
-  let userId = "";
-  if (token) {
-    userId = jwt_decode(token).data.objId;
-  }
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/React-Rent-Go-Where/#/">
-          Rent-Go-Where
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              {token ? (
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href={`/React-Rent-Go-Where/#/api/v1/board/show_properties/${userId}`}
-                >
-                  Favourite
-                </a>
-              ) : (
-                ""
-              )}
-            </li>
-            <li className="nav-item">
-              {token ? (
-                <a className="nav-link active" aria-current="page" href="#">
-                  Potential Housemate
-                </a>
-              ) : (
-                ""
-              )}
-            </li>
+	const token = localStorage.getItem("user_token");
+	let userId = "";
+	if (token) {
+		userId = jwt_decode(token).data.objId;
+	}
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="/React-Rent-Go-Where-GA-Project-3/#/">
+					Rent-Go-Where
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							{token ? (
+								<a
+									className="nav-link active"
+									aria-current="page"
+									href={`/React-Rent-Go-Where-GA-Project-3/#/api/v1/board/show_properties/${userId}`}>
+									Favourite
+								</a>
+							) : (
+								""
+							)}
+						</li>
+						<li className="nav-item">
+							{token ? (
+								<a className="nav-link active" aria-current="page" href="#">
+									Potential Housemate
+								</a>
+							) : (
+								""
+							)}
+						</li>
 
-            <li className="nav-item">
-              {token ? (
-                <a
-                  className="nav-link active"
-                  aria-current="page"
-                  href={`/React-Rent-Go-Where/#/api/v1/app/create_properties`}
-                >
-                  Add properties
-                </a>
-              ) : (
-                ""
-              )}
-            </li>
-          </ul>
+						<li className="nav-item">
+							{token ? (
+								<a
+									className="nav-link active"
+									aria-current="page"
+									href={`/React-Rent-Go-Where-GA-Project-3/#/api/v1/app/create_properties`}>
+									Add properties
+								</a>
+							) : (
+								""
+							)}
+						</li>
+					</ul>
 
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            {token ? (
-              <Dropdown>
-                <Dropdown.Toggle variant="dark" id="dropdown-basic">
-                  UserName
-                </Dropdown.Toggle>
+					<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+						{token ? (
+							<Dropdown>
+								<Dropdown.Toggle variant="dark" id="dropdown-basic">
+									UserName
+								</Dropdown.Toggle>
 
-                <Dropdown.Menu>
-                  <Dropdown.Item>
-                    <Link to="/api/v1/profile">Profile</Link>
-                  </Dropdown.Item>
-                  <Dropdown.Item>
-                    <a onClick={logoutHandler}>
-                      <Link to="/">Logout</Link>
-                    </a>
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            ) : (
-              <>
-                <li className="nav-link">
-                  <Link to="/api/v1/auth/register">Register</Link>
-                </li>
-                <li className="nav-link">
-                  <Link to="/api/v1/auth/login">Login</Link>
-                </li>
-              </>
-            )}
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+								<Dropdown.Menu>
+									<Dropdown.Item>
+										<Link to="/api/v1/profile">Profile</Link>
+									</Dropdown.Item>
+									<Dropdown.Item>
+										<a onClick={logoutHandler}>
+											<Link to="/">Logout</Link>
+										</a>
+									</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						) : (
+							<>
+								<li className="nav-link">
+									<Link to="/api/v1/auth/register">Register</Link>
+								</li>
+								<li className="nav-link">
+									<Link to="/api/v1/auth/login">Login</Link>
+								</li>
+							</>
+						)}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default SiteHeader;
